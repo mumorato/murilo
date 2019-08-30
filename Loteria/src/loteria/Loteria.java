e08f19051edbf39650fedf41abcbc5a0209f7152
Murilo <murilomorato@gmail.com>
2019-08-29 22:03:53 -0300

    funcoes
    incluido

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -8,16 +8,25 @@ public class Loteria {
        Scanner in = new Scanner (System.in);
         System.out.println("Digite o numero");
         for (int i = 0; i < 6; i++) {
-            
-            
-            
+            int num = -1;
+            while (num == -1) {
+            num = funcao(in, i);
+            }
         }
-        
-
-
-
-
-// TODO code application logic here
     }
+    public static int funcao(Scanner in, int i){
+        System.out.println("Digite o " + (i+1)+ "ro. numero.");
+        int num = in.nextInt();
+        
+        if (num >= 1 && num <= 60) {
+            return num;
+        }
+        else{
+            return -1;
+        } 
+            
+        
+    }
+    
     
 }
