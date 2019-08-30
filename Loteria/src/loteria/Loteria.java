e516312d9d74fb1832aacffcd90bfdd5516354db
Murilo <murilomorato@gmail.com>
2019-08-29 21:40:32 -0300

    primeiras linhas
    apagado coments e iniciado

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package loteria;
 
-/**
- *
- * @author macbook
- */
+import java.util.Scanner;
 public class Loteria {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner in = new Scanner (System.in);
+        System.out.println("Digite o numero");
+        for (int i = 0; i < 6; i++) {
+            
+            
+            
+        }
+        
+
+
+
+
+// TODO code application logic here
     }
     
 }
