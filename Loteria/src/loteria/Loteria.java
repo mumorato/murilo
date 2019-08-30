ff7d25945182621c2298c7c25ff2c4ced3af8d67
Murilo <murilomorato@gmail.com>
2019-08-29 22:41:01 -0300

    finalizado Loteria
    Com repetidos

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -5,28 +5,42 @@ package loteria;
 import java.util.Scanner;
 public class Loteria {
     public static void main(String[] args) {
-       Scanner in = new Scanner (System.in);
+        
+//  CRIANDO VETOR    int valores[] = new int [6];  E JÁ POPULANDO:       
+        int valores[] = new int [] {0,0,0,0,0,0};
+       
+        Scanner in = new Scanner (System.in);
         System.out.println("Digite o numero");
         for (int i = 0; i < 6; i++) {
             int num = -1;
             while (num == -1) {
-            num = funcao(in, i);
+                num = funcao(in, i);
+                for (int j = 0; j < i; j++) {
+                    if (valores[j] == num) {
+                        num = -1;
+                    }
+                }
             }
-        }
+            valores[i]=num;
+            imprimeVetor (valores);
+        }       
     }
-    public static int funcao(Scanner in, int i){
-        System.out.println("Digite o " + (i+1)+ "ro. numero.");
+    
+    public static int funcao(Scanner in, int i) {
+        System.out.println("Digite o " + (i + 1) + "ro. numero.");
         int num = in.nextInt();
-        
+
         if (num >= 1 && num <= 60) {
             return num;
-        }
-        else{
+        } else {
             return -1;
-        } 
-            
-        
+        }
     }
     
-    
+    public static void imprimeVetor (int[] vet) {
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(vet[i]+" ");
+        }
+        System.out.println("");
+    }
 }
