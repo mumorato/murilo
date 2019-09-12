4033b3bd346c505c4a0e7c18283ea014f61401ab
Murilo <murilomorato@gmail.com>
2019-09-11 22:38:47 -0300

    atualizacao
    cods

--- POOcalculadora/src/Calculadora.java
+++ POOcalculadora/src/Calculadora.java
@@ -1,22 +1,43 @@
 
 public class Calculadora {
-   public int somar(int a, int b){
-    return a+b;
- }
-   public float somar(float a, float b){
-    return a+b;
- }
-   
-   
-   public int subtrair(int a, int b){
-       return a-b;
-   }
-   public float subtrair(float a, float b){
-       return a-b;
-   }   
-   
-   
-   
-   
-   
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public float somar(float a, float b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public float multiplicar(float a, float b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        return a / b;
+    }
+
+    public float dividir(float a, float b) {
+        return a / b;
+    }
+
+    public int porcentagem(int a, int b) {
+        return (a / 10) * b;
+    }
+
+    public float porcentagem(float a, float b) {
+        return (a / 10) * b;
+    }
 }
--- POOcalculadora/src/Teste.java
+++ POOcalculadora/src/Teste.java
@@ -1,13 +1,24 @@
 
 public class Teste {
+
     public static void main(String[] args) {
-        Calculadora calc = new Calculadora ();
-        
-        System.out.println("Resultado soma int " +calc.somar(5,2));
-        System.out.println("Resultado soma float " +calc.somar(5.4f,2.3f));
-        
+        Calculadora calc = new Calculadora();
+
+        System.out.println("Resultado soma int " + calc.somar(5, 2));
+        System.out.println("Resultado soma float " + calc.somar(5.4f, 2.3f));
+
         System.out.println("Resultado subtracao int " + calc.subtrair(10, 3));
         System.out.println("Resultado subtracao int " + calc.subtrair(10.5f, 3.2f));
+
+        System.out.println("Resultado multiplicacao int " + calc.multiplicar(10, 3));
+        System.out.println("Resultado multiplicacao int " + calc.multiplicar(10.5f, 3.2f));
+
+        System.out.println("Resultado divisao int " + calc.dividir(10, 3));
+        System.out.println("Resultado divisao int " + calc.dividir(10.5f, 3.2f));
+
+        System.out.println("Resultado porcentagem int " + calc.dividir(10, 300));
+        System.out.println("Resultado porcentagem int " + calc.dividir(10f, 300.50f));
+
     }
-  
+
 }
