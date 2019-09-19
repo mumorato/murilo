2cd13443c5f2c76a370602f7548ab560138f8c7a
Murilo <murilomorato@gmail.com>
2019-09-18 22:34:15 -0300

    Exercicios, inicio fk
    atividade

---
+++ ModeloDadosCupomFiscal/src/Empresa.java
@@ -0,0 +1,70 @@
+
+public class Empresa {
+    
+    private int id;
+    private String nome;
+    private String cnpj;
+    private String ie;
+    private String rua;
+    private String bairro;
+    private String numero;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    
+    
+}
---
+++ ModeloDadosCupomFiscal/src/Venda.java
@@ -0,0 +1,15 @@
+
+public class Venda {
+
+    private int id;
+    private String datavenda;
+    private int numerovenda;
+    private int serievenda;
+    private String chaveacesso;
+    private String dataprotocolo;
+    private float desconto;
+   // private int empresa_id; fk
+    
+
+    
+}
