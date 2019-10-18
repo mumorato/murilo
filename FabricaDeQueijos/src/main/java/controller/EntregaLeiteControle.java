dc443fd6b11bb1e8b53c6c8bd9b966b4a098e3eb
Murilo <murilomorato@gmail.com>
2019-10-17 22:24:41 -0300

    EntregaLeite

---
+++ FabricaDeQueijos/src/main/java/controller/EntregaLeiteControle.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Fornecedor;
+
+/**
+ *
+ * @author macbook
+ */
+public class EntregaLeiteControle {
+
+    public List<Fornecedor> PesquisarFornecedores(String pesquisa) {
+        List<Fornecedor> fornecedores = new ArrayList<Fornecedor>();
+        return fornecedores;
+    }
+
+    public boolean salvar(Fornecedor f) {
+        return false;
+    }
+}
---
+++ FabricaDeQueijos/src/main/java/controller/FornecedorController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import model.Fornecedor;
+
+/**
+ *
+ * @author macbook
+ */
+public class FornecedorController {
+    public boolean salvar(Fornecedor f){
+        return false;
+    }
+}
